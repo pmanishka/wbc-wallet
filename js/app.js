0e2233345b84c25c57c0931f30d61a8642e48347
unknown <p.grover@intouchgroup.in>
2018-02-09 17:13:23 +0530

    Update file

--- js/app.js
+++ js/app.js
@@ -27,3 +27,38 @@
         app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
     }, false);
 })(window);
+
+
+$(document).ready(function() {
+    document.addEventListener("deviceready", onDeviceReady, false);
+});
+
+function onDeviceReady() {      
+     $('#scan').click( function() 
+        {
+          cordova.plugins.barcodeScanner.scan(
+          function (result) {
+              alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);            
+          }, 
+          function (error) {
+              alert("Scanning failed: " + error);
+          });
+        }
+     );
+}
+function hasCameraPermission() {
+    cordova.plugins.barcodeScanner.hasCameraPermission(
+      function(result) {
+        // if this is 'false' you probably want to call 'requestCameraPermission' now
+        alert(result);
+      }
+    )
+  }
+
+  function requestCameraPermission() {
+    // no callbacks required as this opens a popup which returns async
+    cordova.plugins.barcodeScanner.requestCameraPermission();
+  }
