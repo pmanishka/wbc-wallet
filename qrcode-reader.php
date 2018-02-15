341997630a5a678fec0f3056a2f529eac416f4f7
unknown <p.grover@intouchgroup.in>
2018-02-15 18:25:34 +0530

    Update file

--- qrcode-reader.php
+++ qrcode-reader.php
@@ -1,38 +1,6 @@
 <?php
 
-print_r($_GET);
+print_r($_REQUEST);
 exit();
-$string = <<<XML
-  <PrintLetterBarcodeData 
-    uid="725733706873" 
-    name="RAVINDER KUMAR" 
-    gender="M" 
-    yob="1996" 
-    co="S/O KAKA RAM" 
-    house="460A" 
-    street="WARD NO. 6" 
-    lm="NA" 
-    loc="NA" 
-    vtc="Nanyola (292)" 
-    po="Naneola" 
-    dist="Ambala" 
-    state="Haryana" 
-    pc="134003"
-  />
-XML;
-
-
-$xml = simplexml_load_string($string);
-$attribs = $xml->attributes();
-// convert the '$attribs' to an array
-foreach($attribs as $key=>$val) {
-    $arrayOfAttribs[(string)$key] = "'".(string)$val."'";
-	//$arrayOfAttribs['uid'];
-}
-echo json_encode($arrayOfAttribs);
-
-//$namesOfColumns = implode(",", array_keys($arrayOfAttribs));
-//$valuesOfColumns = implode(",", array_values($arrayOfAttribs));
-
 
 ?>
