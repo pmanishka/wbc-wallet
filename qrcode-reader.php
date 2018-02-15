677ca5889ad1158b60c1fc631e74a987169e3f26
unknown <p.grover@intouchgroup.in>
2018-02-15 18:36:48 +0530

    update

--- qrcode-reader.php
+++ qrcode-reader.php
@@ -1,6 +1,24 @@
 <?php
 
-print_r($_REQUEST);
-exit();
+$xml =  $_REQUEST;
+//exit();
+
+$string = <<<XML . $xml
+XML;
+
+
+$xml = simplexml_load_string($string);
+$attribs = $xml->attributes();
+// convert the '$attribs' to an array
+foreach($attribs as $key=>$val) {
+    $arrayOfAttribs[(string)$key] = "'".(string)$val."'";
+	//$arrayOfAttribs['uid'];
+}
+echo json_encode($arrayOfAttribs);
+
+//$namesOfColumns = implode(",", array_keys($arrayOfAttribs));
+//$valuesOfColumns = implode(",", array_values($arrayOfAttribs));
+
+
 
 ?>
